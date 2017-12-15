a912c56c663be5a931e3640553c541afa70afb89
Rita Kurban <ritakurban@Eugenes-MacBook-Pro.local>
2017-12-15 21:48:06 +0900

    Improved formatting

--- actselectw/actselectw.py
+++ actselectw/actselectw.py
@@ -1,17 +1,20 @@
-"""Create a class to define possible activities """
 class activity:
+    """Create a class to define possible activities """
+
     def __init__(self, name, start, finish, weight):
         self.name = name
         self.start = start
         self.finish = finish
         self.weight = weight
 
-"""Return the end time of the activity """
+
 def fin(activity):
+    """Return the end time of the activity """
     return activity.finish
 
-"""Find the previous compatible activity using binary search. """
+
 def search(activity, current):
+    """Find the previous compatible activity using binary search. """
     mini = 0
     maxi = current - 1
     while mini <= maxi:
@@ -25,32 +28,33 @@ def search(activity, current):
             maxi = avg - 1
     return None
 
-"""Return the max. weight and the sequence of activities. """
+
 def actselectw(act):
-    """Sort the activities in increasing order. """
+    """Return the max. weight and the sequence of activities. """
+    # Sort the activities in increasing order.
     activity = sorted(act, key=fin)
     seq = 0
-    """Find the length of the list. """
+    # Find the length of the list.
     length = len(activity)
-    """Create a table of max weights. """
+    #Create a table of max weights.
     table = [0 for i in range(length)]
-    """Assign the first activity to the first cell of the table. """
+    #Assign the first activity to the first cell of the table.
     table[0] = activity[0].weight
     parent = [1]
-    """Dynamic programming part to calculate the highest weight. """
+    #Dynamic programming part to calculate the highest weight.
     for i in range(1, length):
         weights = activity[i].weight
         prev = search(activity, i)
         if prev != None:
             weights += table[prev]
-        if weights > table[i-1]:
+        if weights > table[i - 1]:
             table[i] = weights
             parent.append(1)
         else:
             table[i] = table[i - 1]
             parent.append(0)
 
-    """Find the sequence of activities. """
+    #Find the sequence of activities.
     seq = []
     temp = 0
     i = len(parent) - 1
@@ -62,4 +66,5 @@ def actselectw(act):
         else:
             i -= 1
     seq = seq[::-1]
-    return "Max weight is " + str(table[-1]) + ". You should pick activities " + str(seq) + "."
+    return "Max weight is " + str(
+        table[-1]) + ". You should pick activities " + str(seq) + "."
